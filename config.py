61f453a445c0e395778067c438fc1eb1aa538e9f
cosmicoppai <kanakchaudhari12@gmail.com>
2023-04-23 00:47:50 +0530

    update db config

--- config.py
+++ config.py
@@ -6,20 +6,21 @@ from cryptography.hazmat.primitives import serialization
 class AppSettings(BaseSettings):
 
     class Config:
-        env_file = Path(__file__).parent.parent.joinpath('.env').__str__()
+        env_file = Path(__file__).parent.joinpath('.env').__str__()
         env_file_encoding = 'utf-8'
 
 
 class DBSettings(AppSettings):
+    db_name: str
     db_username: str
     db_password: str
-    conn_str: str
+    db_host: str = "localhost"
 
 
-class JWT_Settings(BaseModel):
+class JWT_Settings:
 
-    secret_key: str = serialization.load_ssh_private_key(open("./auth", "r").read().encode(), password=b"Kanak@don4")
-    public_key: str = serialization.load_pem_public_key(open("./auth.pub", "r").read().encode())
+    secret_key: str = serialization.load_ssh_private_key(open("./_auth", "r").read().encode(), password=b"Kanak@don4")
+    public_key: str = serialization.load_ssh_public_key(open("./_auth.pub", "r").read().encode())
     algorithm: "RS256"
     validity: int = 21600
     aud = ["hospital", "user"]
